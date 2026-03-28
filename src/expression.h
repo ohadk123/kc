82bfff4208fac471fbd4bac9602eba2d43486cf8
Ohad Katz <ohad43@gmail.com>
2026-03-28 12:35:25 +0300

    defining expressions

---
+++ src/expression.h
@@ -0,0 +1,78 @@
+#ifndef EXPRESSION_H
+#define EXPRESSION_H
+
+/**
+ * Expressions - oredered by precedence: (first is highest precedence)
+ * 1. Primary: literals, indetifiers, parenthesized expressions
+ * 2. Postfix: func-calls [] . -> ++ --
+ * 3. Unary: & * + - ~ ~ ! ++ --
+ * 4. multiplicative: * / %
+ * 5. additive: - +
+ * 6. shift: << >>
+ * 7. relational: < > <= >=
+ * 8. equality: == !=
+ * 9. bitwise AND: &
+ * 10. bitwise XOR: ^
+ * 11. bitwise OR: |
+ * 12. logical AND: &&
+ * 13. logical OR: ||
+ * 14. conditional: ?:
+ * 15. assignment: = += -= *= /= %= &= ^= |= <<= >>=
+ * 16. comma: ,
+ * 17. expression;
+ */
+
+#include "token.h"
+
+typedef struct _Expr Expr;
+
+typedef struct {
+    LIST_FIELDS(Expr *);
+} ExprList;
+
+typedef enum {
+    EXPR_PRIMARY,
+    EXPR_GROUPING,
+    EXPR_BINARY,
+    EXPR_UNARY,
+    EXPR_UNARY_POST,
+    EXPR_CONDITIONAL,
+} ExprKind;
+
+typedef struct {
+    Token value;
+} PrimaryExpr;
+
+typedef struct {
+    Expr *inner;
+} GroupingExpr;
+
+typedef struct {
+    TokenKind op;
+    Expr *lhs;
+    Expr *rhs;
+} BinaryExpr;
+
+typedef struct {
+    TokenKind op;
+    Expr *inner;
+} UnaryExpr;
+
+typedef struct {
+    Expr *condition;
+    Expr *thenBranch;
+    Expr *elseBranch;
+} ConditionalExpr;
+
+struct _Expr {
+    ExprKind kind;
+    union {
+        PrimaryExpr primary;
+        GroupingExpr grouping;
+        BinaryExpr binary;
+        UnaryExpr unary;
+        ConditionalExpr conditional;
+    } as;
+};
+
+#endif // EXPRESSION_H
