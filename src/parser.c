45b0a65d9b22f0ea4ade640b8327c9fa8d28dc66
Ohad Katz <ohad43@gmail.com>
2026-05-02 17:04:34 +0300

    formatting

--- src/parser.c
+++ src/parser.c
@@ -389,16 +389,18 @@ Stmt *return_stmt(Parser *p) {
 }
 
 Stmt *statement(Parser *p) {
-    if (match_types(p)) return var_stmt(p);
-    if (match(p, TOK_FOR)) return for_stmt(p);
-    if (match(p, TOK_WHILE)) return while_stmt(p);
-    if (match(p, TOK_IF)) return if_stmt(p);
+    if (match_types(p))           return var_stmt(p);
+    if (match(p, TOK_FOR))        return for_stmt(p);
+    if (match(p, TOK_WHILE))      return while_stmt(p);
+    if (match(p, TOK_IF))         return if_stmt(p);
     if (match(p, TOK_LEFT_BRACE)) return block_stmt(p);
-    if (match(p, TOK_BREAK)) return break_stmt(p);
-    if (match(p, TOK_CONTINUE)) return continue_stmt(p);
-    if (match(p, TOK_RETURN)) return return_stmt(p);
+    if (match(p, TOK_BREAK))      return break_stmt(p);
+    if (match(p, TOK_CONTINUE))   return continue_stmt(p);
+    if (match(p, TOK_RETURN))     return return_stmt(p);
 
     Location loc = peek(p).loc;
+    if (match(p, TOK_SEMICOLON))  return stmt_make_null(loc);
+
     Expr *expr = expression(p);
     expect(p, TOK_SEMICOLON, "Expected ';' after expression");
     return stmt_make_expr(expr, loc);
