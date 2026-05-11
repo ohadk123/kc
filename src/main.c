fdf9ad9ad0eedc934295980de62b2e17e3a1c561
Ohad Katz <ohad87@proton.me>
2026-05-11 11:20:12 +0300

    test commit

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: kc <file>\n");
         exit(1);
-    }
+    };
 
     TranslationUnit unit = (TranslationUnit){
         .fileName = str_from_cstr(argv[1]),
