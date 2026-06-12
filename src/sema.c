932abad6c649f7c131f9bf0257170d005eb8e668
Ohad Katz <ohad87@proton.me>
2026-06-12 10:30:58 +0300

    rename a->c (for [c]hecker)

--- src/sema.c
+++ src/sema.c
@@ -47,7 +47,7 @@ typedef struct {
     Scope *curr;
 } Checker;
 
-static Type *check_expr(Checker *a, Expr *e);
+static Type *check_expr(Checker *c, Expr *e);
 
 static TokenKind int_lit_to_token_kind(Token integerTok, String fileName) {
     assert(integerTok.kind == TOK_INTEGER_LITERAL);
@@ -59,15 +59,15 @@ static TokenKind int_lit_to_token_kind(Token integerTok, String fileName) {
     compile_error(fileName, integerTok.loc, "integer literal is too large");
 }
 
-static Type *check_primary(Checker *a, Expr *e) {
+static Type *check_primary(Checker *c, Expr *e) {
     PrimaryExpr prime = e->as.primary;
 
     switch (prime.value.kind) {
         case TOK_IDENTIFIER: {
             assert(prime.value.kind == TOK_IDENTIFIER);
-            Type *type = get_ident_type(a->curr, prime.value.as.identifier);
+            Type *type = get_ident_type(c->curr, prime.value.as.identifier);
             if (!type)
-                compile_error(a->unit->fileName, e->loc, "unkown symbol '%.*s'", strf(prime.value.as.identifier));
+                compile_error(c->unit->fileName, e->loc, "unkown symbol '%.*s'", strf(prime.value.as.identifier));
             return type;
         }
         case TOK_STRING_LITERAL: TODO("Evaluate string type");
@@ -75,17 +75,17 @@ static Type *check_primary(Checker *a, Expr *e) {
         case TOK_FALSE:          return type_make_primitive_from_token(TOK_BOOL);
         case TOK_CHAR_LITERAL:   return type_make_primitive_from_token(TOK_U8);
         case TOK_INTEGER_LITERAL:
-            return type_make_primitive_from_token(int_lit_to_token_kind(prime.value, a->unit->fileName));
+            return type_make_primitive_from_token(int_lit_to_token_kind(prime.value, c->unit->fileName));
         case TOK_FLOAT_LITERAL: return type_make_primitive_from_token(TOK_F64);
-        default:                compile_error(a->unit->fileName, e->loc, "Not a type kind (%d)", prime.value.kind);
+        default:                compile_error(c->unit->fileName, e->loc, "Not a type kind (%d)", prime.value.kind);
     }
 }
 
-static Type *check_binary(Checker *a, Expr *e) {
+static Type *check_binary(Checker *c, Expr *e) {
     BinaryExpr bin = e->as.binary;
 
-    Type *lhs = check_expr(a, bin.lhs);
-    Type *rhs = check_expr(a, bin.rhs);
+    Type *lhs = check_expr(c, bin.lhs);
+    Type *rhs = check_expr(c, bin.rhs);
     Type *type = compare_types(lhs, rhs);
     if (!type) TODO("Type error message");
 
@@ -104,17 +104,17 @@ static Type *check_binary(Checker *a, Expr *e) {
     return type;
 }
 
-static Type *check_unary(Checker *a, Expr *e) {
+static Type *check_unary(Checker *c, Expr *e) {
     UnaryExpr unary = e->as.unary;
 
-    Type *type = check_expr(a, unary.inner);
+    Type *type = check_expr(c, unary.inner);
 
     if (unary.op == TOK_BANG && type->kind != TYPE_PRIMITIVE && type->as.primitive != TYPE_BOOL)
-        compile_error(a->unit->fileName, e->loc, "cannot negate non-bool value");
+        compile_error(c->unit->fileName, e->loc, "cannot negate non-bool value");
 
     if (unary.op == TOK_STAR) {
         if (type->kind != TYPE_POINTER)
-            compile_error(a->unit->fileName, e->loc, "cannot dereference non-pointer value");
+            compile_error(c->unit->fileName, e->loc, "cannot dereference non-pointer value");
         type = type->as.pointer;
     } else if (unary.op == TOK_AMPERSAND) {
         type = type_make_pointer(type);
@@ -142,51 +142,51 @@ static bool is_lvalue(Expr *e) {
     return false;
 }
 
-static Type *check_assign(Checker *a, Expr *e) {
+static Type *check_assign(Checker *c, Expr *e) {
     AssignExpr ass = e->as.assignment;
 
     // assignee must be a variable identifier
     Expr *assginee = e->as.assignment.lhs;
-    if (!is_lvalue(assginee)) compile_error(a->unit->fileName, e->loc, "expression is not assignable");
+    if (!is_lvalue(assginee)) compile_error(c->unit->fileName, e->loc, "expression is not assignable");
 
-    Type *lhs = check_expr(a, ass.lhs);
-    Type *rhs = check_expr(a, ass.rhs);
+    Type *lhs = check_expr(c, ass.lhs);
+    Type *rhs = check_expr(c, ass.rhs);
     return compare_types(lhs, rhs);
 }
 
-static Type *check_conditional(Checker *a, Expr *e) {
+static Type *check_conditional(Checker *c, Expr *e) {
     ConditionalExpr cond = e->as.conditional;
 
-    Type *condType = check_expr(a, cond.condition);
+    Type *condType = check_expr(c, cond.condition);
     if (condType->kind != TYPE_PRIMITIVE || condType->as.primitive != TYPE_BOOL)
-        compile_error(a->unit->fileName, e->loc, "ternary expression condition is not boolean");
+        compile_error(c->unit->fileName, e->loc, "ternary expression condition is not boolean");
 
-    Type *thenBranch = check_expr(a, cond.thenBranch);
-    Type *elseType = check_expr(a, cond.elseBranch);
+    Type *thenBranch = check_expr(c, cond.thenBranch);
+    Type *elseType = check_expr(c, cond.elseBranch);
     return compare_types(thenBranch, elseType);
 }
 
-static Type *check_func_call(Checker *a, Expr *e) {
+static Type *check_func_call(Checker *c, Expr *e) {
     FuncCallExpr funcCall = e->as.funcCall;
 
-    Type *funcDeclType = check_expr(a, funcCall.func);
-    if (funcDeclType->kind != TYPE_FUNC) compile_error(a->unit->fileName, e->loc, "expression is not a function");
+    Type *funcDeclType = check_expr(c, funcCall.func);
+    if (funcDeclType->kind != TYPE_FUNC) compile_error(c->unit->fileName, e->loc, "expression is not a function");
 
     FuncType funcType = funcDeclType->as.func;
     if (funcType.params.len > funcCall.args.len)
-        compile_error(a->unit->fileName, e->loc, "too few arguments to function call, expected %zu, got %zu",
+        compile_error(c->unit->fileName, e->loc, "too few arguments to function call, expected %zu, got %zu",
                       funcType.params.len, funcCall.args.len);
     else if (funcType.params.len < funcCall.args.len)
-        compile_error(a->unit->fileName, e->loc, "too many arguments to function call, expected %zu, got %zu",
+        compile_error(c->unit->fileName, e->loc, "too many arguments to function call, expected %zu, got %zu",
                       funcType.params.len, funcCall.args.len);
 
     for (size_t i = 0; i < funcType.params.len; i++) {
-        Type *argType = check_expr(a, funcCall.args.arr[i]);
+        Type *argType = check_expr(c, funcCall.args.arr[i]);
         Type *paramType = funcType.params.arr[i]->as.var.type;
         if (!compare_types(argType, paramType)) {
             String argTypeStr = type_to_string(argType);
             String paramTypeStr = type_to_string(paramType);
-            compile_error(a->unit->fileName, e->loc, "cannot pass argument of type '%.*s' to parameter of type '%.*s'",
+            compile_error(c->unit->fileName, e->loc, "cannot pass argument of type '%.*s' to parameter of type '%.*s'",
                           strf(argTypeStr), strf(paramTypeStr));
         }
     }
@@ -217,174 +217,174 @@ static bool is_integer(Type *t) {
     }
 }
 
-static Type *check_index(Checker *a, Expr *e) {
+static Type *check_index(Checker *c, Expr *e) {
     IndexExpr index = e->as.index;
 
-    Type *arrayType = check_expr(a, index.array);
+    Type *arrayType = check_expr(c, index.array);
     if (arrayType->kind != TYPE_ARRAY && arrayType->kind != TYPE_POINTER)
-        compile_error(a->unit->fileName, e->loc, "cannot index value which is not array nor pointer");
+        compile_error(c->unit->fileName, e->loc, "cannot index value which is not array nor pointer");
 
-    Type *indexType = check_expr(a, index.index);
-    if (!is_integer(indexType)) compile_error(a->unit->fileName, e->loc, "array index is not an integer");
+    Type *indexType = check_expr(c, index.index);
+    if (!is_integer(indexType)) compile_error(c->unit->fileName, e->loc, "array index is not an integer");
 
     return arrayType->kind == TYPE_ARRAY ? arrayType->as.array.elementType : arrayType->as.pointer;
 }
 
-static Type *check_expr(Checker *a, Expr *e) {
+static Type *check_expr(Checker *c, Expr *e) {
     Type *type = 0;
 
     switch (e->kind) {
-        case EXPR_PRIMARY:     type = check_primary(a, e); break;
-        case EXPR_GROUPING:    type = check_expr(a, e->as.grouping.inner); break;
-        case EXPR_BINARY:      type = check_binary(a, e); break;
-        case EXPR_UNARY:       type = check_unary(a, e); break;
-        case EXPR_ASSIGN:      type = check_assign(a, e); break;
-        case EXPR_UNARY_POST:  type = check_expr(a, e->as.unary.inner); break;
-        case EXPR_CONDITIONAL: type = check_conditional(a, e); break;
-        case EXPR_FUNC_CALL:   type = check_func_call(a, e); break;
-        case EXPR_INDEX:       type = check_index(a, e);
+        case EXPR_PRIMARY:     type = check_primary(c, e); break;
+        case EXPR_GROUPING:    type = check_expr(c, e->as.grouping.inner); break;
+        case EXPR_BINARY:      type = check_binary(c, e); break;
+        case EXPR_UNARY:       type = check_unary(c, e); break;
+        case EXPR_ASSIGN:      type = check_assign(c, e); break;
+        case EXPR_UNARY_POST:  type = check_expr(c, e->as.unary.inner); break;
+        case EXPR_CONDITIONAL: type = check_conditional(c, e); break;
+        case EXPR_FUNC_CALL:   type = check_func_call(c, e); break;
+        case EXPR_INDEX:       type = check_index(c, e);
     }
 
     e->type = type;
     return type;
 }
 
-static void check_stmt(Checker *a, Stmt *s);
+static void check_stmt(Checker *c, Stmt *s);
 
-static void check_var(Checker *a, Stmt *s) {
-    Stmt *symbol = find_symbol(a->curr, s->as.var.name.as.identifier);
+static void check_var(Checker *c, Stmt *s) {
+    Stmt *symbol = find_symbol(c->curr, s->as.var.name.as.identifier);
     if (symbol) {
-        compile_error(a->unit->fileName, s->loc, "symbol '%.*s' already delcared before at [%.*s:%zu:%zu]",
-                      strf(s->as.var.name.as.identifier), strf(a->unit->fileName), s->loc.line, s->loc.col);
+        compile_error(c->unit->fileName, s->loc, "symbol '%.*s' already delcared before at [%.*s:%zu:%zu]",
+                      strf(s->as.var.name.as.identifier), strf(c->unit->fileName), s->loc.line, s->loc.col);
     }
 
-    hm_insert(&a->curr->symbols, s->as.var.name.as.identifier, s);
+    hm_insert(&c->curr->symbols, s->as.var.name.as.identifier, s);
 
     if (check_primitive(s->as.var.type, TYPE_VOID))
-        compile_error(a->unit->fileName, s->loc, "variable cannot be of type 'void'");
+        compile_error(c->unit->fileName, s->loc, "variable cannot be of type 'void'");
 
     if (s->as.var.init) {
-        Type *initType = check_expr(a, s->as.var.init);
+        Type *initType = check_expr(c, s->as.var.init);
         if (!compare_types(initType, s->as.var.type)) TODO("Type error message for variable initializer");
     }
 }
 
-static void check_block(Checker *a, Stmt *s) {
+static void check_block(Checker *c, Stmt *s) {
     Scope next = (Scope){
-        .above = a->curr,
+        .above = c->curr,
         .symbols = {0},
     };
 
-    a->curr = &next;
+    c->curr = &next;
 
-    for (size_t i = 0; i < s->as.block.block.len; i++) check_stmt(a, s->as.block.block.arr[i]);
+    for (size_t i = 0; i < s->as.block.block.len; i++) check_stmt(c, s->as.block.block.arr[i]);
 
-    a->curr = a->curr->above;
+    c->curr = c->curr->above;
 }
 
-static void check_while(Checker *a, Stmt *s) {
-    if (!check_primitive(check_expr(a, s->as.whileS.condition), TYPE_BOOL))
-        compile_error(a->unit->fileName, s->loc, "while condition is not a boolean expression");
-    a->curr->inLoop = true;
-    check_stmt(a, s->as.whileS.body);
-    a->curr->inLoop = false;
+static void check_while(Checker *c, Stmt *s) {
+    if (!check_primitive(check_expr(c, s->as.whileS.condition), TYPE_BOOL))
+        compile_error(c->unit->fileName, s->loc, "while condition is not a boolean expression");
+    c->curr->inLoop = true;
+    check_stmt(c, s->as.whileS.body);
+    c->curr->inLoop = false;
 }
 
-static void check_if(Checker *a, Stmt *s) {
-    if (!check_primitive(check_expr(a, s->as.ifS.condition), TYPE_BOOL)) {
-        compile_error(a->unit->fileName, s->loc, "if condition is not a boolean expression");
+static void check_if(Checker *c, Stmt *s) {
+    if (!check_primitive(check_expr(c, s->as.ifS.condition), TYPE_BOOL)) {
+        compile_error(c->unit->fileName, s->loc, "if condition is not a boolean expression");
     }
-    check_stmt(a, s->as.ifS.thenBranch);
-    if (s->as.ifS.elseBranch) check_stmt(a, s->as.ifS.elseBranch);
+    check_stmt(c, s->as.ifS.thenBranch);
+    if (s->as.ifS.elseBranch) check_stmt(c, s->as.ifS.elseBranch);
 }
 
-static void check_for(Checker *a, Stmt *s) {
+static void check_for(Checker *c, Stmt *s) {
     Scope forScope = (Scope){
-        .above = a->curr,
+        .above = c->curr,
         .inLoop = true,
     };
-    a->curr = &forScope;
+    c->curr = &forScope;
 
-    if (s->as.forS.initializer) check_stmt(a, s->as.forS.initializer);
+    if (s->as.forS.initializer) check_stmt(c, s->as.forS.initializer);
     if (s->as.forS.condition) {
-        Type *condType = check_expr(a, s->as.forS.condition);
+        Type *condType = check_expr(c, s->as.forS.condition);
         if (!check_primitive(condType, TYPE_BOOL))
-            compile_error(a->unit->fileName, s->loc, "for loop condition is not a boolean expression");
+            compile_error(c->unit->fileName, s->loc, "for loop condition is not a boolean expression");
     }
-    if (s->as.forS.increment) check_expr(a, s->as.forS.increment);
-    check_stmt(a, s->as.forS.body);
+    if (s->as.forS.increment) check_expr(c, s->as.forS.increment);
+    check_stmt(c, s->as.forS.body);
 
-    a->curr = a->curr->above;
+    c->curr = c->curr->above;
 }
 
-static void check_return(Checker *a, Stmt *s) {
-    Scope *retScope = a->curr;
+static void check_return(Checker *c, Stmt *s) {
+    Scope *retScope = c->curr;
     while (retScope && !retScope->retType) retScope = retScope->above;
-    if (!retScope) compile_error(a->unit->fileName, s->loc, "return statement not in a function");
+    if (!retScope) compile_error(c->unit->fileName, s->loc, "return statement not in a function");
 
     if (s->as.returnS.retVal) {
-        Type *retValType = check_expr(a, s->as.returnS.retVal);
+        Type *retValType = check_expr(c, s->as.returnS.retVal);
         if (!compare_types(retValType, retScope->retType)) {
             String retValTypeStr = type_to_string(retValType);
             String retScopeTypeStr = type_to_string(retScope->retType);
-            compile_error(a->unit->fileName, s->loc, "cannot return '%.*s' from function returning '%.*s'",
+            compile_error(c->unit->fileName, s->loc, "cannot return '%.*s' from function returning '%.*s'",
                           strf(retValTypeStr), strf(retScopeTypeStr));
         }
     } else if (check_primitive(retScope->retType, TYPE_VOID))
-        compile_error(a->unit->fileName, s->loc, "missing return type");
+        compile_error(c->unit->fileName, s->loc, "missing return type");
 }
 
-static void check_func(Checker *a, Stmt *s) {
+static void check_func(Checker *c, Stmt *s) {
     FuncStmt func = s->as.func;
     Scope funcScope = {
         .symbols = {0},
-        .above = a->curr,
+        .above = c->curr,
         .retType = func.funcType->as.func.retType,
     };
-    a->curr = &funcScope;
+    c->curr = &funcScope;
     StmtList params = func.funcType->as.func.params;
-    for (size_t i = 0; i < params.len; i++) check_var(a, params.arr[i]);
-    for (size_t i = 0; i < func.block.len; i++) check_stmt(a, func.block.arr[i]);
-    a->curr = a->curr->above;
+    for (size_t i = 0; i < params.len; i++) check_var(c, params.arr[i]);
+    for (size_t i = 0; i < func.block.len; i++) check_stmt(c, func.block.arr[i]);
+    c->curr = c->curr->above;
 }
 
-static void check_break(Checker *a, Stmt *s) {
-    Scope *loopScope = a->curr;
+static void check_break(Checker *c, Stmt *s) {
+    Scope *loopScope = c->curr;
     while (loopScope && !loopScope->inLoop) loopScope = loopScope->above;
-    if (!loopScope) compile_error(a->unit->fileName, s->loc, "'break' statement not in loop");
+    if (!loopScope) compile_error(c->unit->fileName, s->loc, "'break' statement not in loop");
 }
 
-static void check_continue(Checker *a, Stmt *s) {
-    Scope *loopScope = a->curr;
+static void check_continue(Checker *c, Stmt *s) {
+    Scope *loopScope = c->curr;
     while (loopScope && !loopScope->inLoop) loopScope = loopScope->above;
-    if (!loopScope) compile_error(a->unit->fileName, s->loc, "'continue' statement not in loop");
+    if (!loopScope) compile_error(c->unit->fileName, s->loc, "'continue' statement not in loop");
 }
 
-static void check_stmt(Checker *a, Stmt *s) {
+static void check_stmt(Checker *c, Stmt *s) {
     switch (s->kind) {
         case STMT_NULL:                                     break;
-        case STMT_VAR:      check_var(a, s);                break;
-        case STMT_EXPR:     check_expr(a, s->as.expr.expr); break;
-        case STMT_BLOCK:    check_block(a, s);              break;
-        case STMT_WHILE:    check_while(a, s);              break;
-        case STMT_DO_WHILE: check_while(a, s);              break;
-        case STMT_IF:       check_if(a, s);                 break;
-        case STMT_FOR:      check_for(a, s);                break;
-        case STMT_RETURN:   check_return(a, s);             break;
-        case STMT_FUNC:     check_func(a, s);               break;
-        case STMT_BREAK:    check_break(a, s);              break;
-        case STMT_CONTINUE: check_continue(a, s);           break;
+        case STMT_VAR:      check_var(c, s);                break;
+        case STMT_EXPR:     check_expr(c, s->as.expr.expr); break;
+        case STMT_BLOCK:    check_block(c, s);              break;
+        case STMT_WHILE:    check_while(c, s);              break;
+        case STMT_DO_WHILE: check_while(c, s);              break;
+        case STMT_IF:       check_if(c, s);                 break;
+        case STMT_FOR:      check_for(c, s);                break;
+        case STMT_RETURN:   check_return(c, s);             break;
+        case STMT_FUNC:     check_func(c, s);               break;
+        case STMT_BREAK:    check_break(c, s);              break;
+        case STMT_CONTINUE: check_continue(c, s);           break;
     }
 }
 
 void semantic_analysis(TranslationUnit *unit) {
-    Checker a = {0};
-    a.curr = &unit->globalSymbols;
-    a.unit = unit;
+    Checker c = {0};
+    c.curr = &unit->globalSymbols;
+    c.unit = unit;
 
     fill_global_symbol_table(unit);
 
     for (size_t i = 0; i < unit->ast.len; i++) {
-        check_stmt(&a, unit->ast.arr[i]);
+        check_stmt(&c, unit->ast.arr[i]);
     }
 }
