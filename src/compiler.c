4b926316c066ace073470424c3d59cb29a8d2433
Ohad Katz <ohad87@proton.me>
2026-06-12 10:25:12 +0300

    cool colors

--- src/compiler.c
+++ src/compiler.c
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 
 __attribute__((__noreturn__)) void compile_error(String fileName, Location place, const char *fmt, ...) {
-    fprintf(stderr, "[%.*s:%zu:%zu] Error: ", (int) fileName.len, fileName.data, place.line, place.col);
+    fprintf(stderr, "[%.*s:%zu:%zu] \033[31;1;4mError:\033[0m ", (int) fileName.len, fileName.data, place.line, place.col);
     va_list args;
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
